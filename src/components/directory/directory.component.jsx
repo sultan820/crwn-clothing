66139a27e029733f6be211c2627c0ecbaecbc577
sultan820 <sultanbutt820@gmail.com>
2019-09-25 23:16:58 +0500

    shop page initial design done

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -14,33 +14,38 @@ class Directory extends React.Component {
           title: "hats",
           imageUrl:
             "https://images.unsplash.com/photo-1514867312438-db0960ee52e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1034&q=80",
-          id: 1
+          id: 1,
+          linkUrl: "hats"
         },
         {
           title: "jackets",
           imageUrl:
             "https://images.unsplash.com/photo-1551488831-00ddcb6c6bd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
-          id: 2
+          id: 2,
+          linkUrl: ""
         },
         {
           title: "sneakers",
           imageUrl:
             "https://images.unsplash.com/photo-1460353581641-37baddab0fa2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80",
-          id: 3
+          id: 3,
+          linkUrl: ""
         },
         {
           title: "womens",
           imageUrl:
             "https://images.unsplash.com/photo-1567401893414-76b7b1e5a7a5?ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
           size: "large",
-          id: 4
+          id: 4,
+          linkUrl: ""
         },
         {
           title: "mens",
           imageUrl:
             "https://images.unsplash.com/photo-1490114538077-0a7f8cb49891?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
           size: "large",
-          id: 5
+          id: 5,
+          linkUrl: ""
         }
       ]
     };
@@ -49,8 +54,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
